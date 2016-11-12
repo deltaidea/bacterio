98ade46716fe4bd487f6bf531d9e7a7cb97f57ec
Nikita Litvin <n@deltaidea.com>
2016-11-12 06:21:22 +0300

    feat(Animal): add back the animals as circles - this time with health

--- src/Animal.js
+++ src/Animal.js
@@ -1,18 +1,18 @@
-class Tile {
+class Animal {
   constructor (x, y, tileSize, game) {
     this.x = x
     this.y = y
-    this.food = game.math.between(0, 50)
+    this.health = game.math.between(200, 255)
     this.sprite = game.add.sprite(tileSize * x, tileSize * y, getBlankBitmap(tileSize, game))
   }
 
   tick () {
-    this.food += 1
-    if (this.food > 255) this.food = 255
+    this.health -= 1
+    if (this.health < 0) this.health = 0
   }
 
   render () {
-    this.sprite.tint = foodToRgb(this.food)
+    this.sprite.tint = healthToRgb(this.health)
   }
 }
 
@@ -20,16 +20,16 @@ let bitmap = null
 function getBlankBitmap (tileSize, game) {
   if (!bitmap) {
     bitmap = game.make.bitmapData(tileSize, tileSize)
-    bitmap.rect(0, 0, tileSize, tileSize, '#ffffff')
+    bitmap.circle(tileSize / 2, tileSize / 2, tileSize / 2, '#ff0000')
   }
   return bitmap
 }
 
-function foodToRgb (food) {
-  // More food - less brightness.
-  let brightness = 255 - food
+function healthToRgb (health) {
+  // More health - less brightness.
+  let brightness = 255 - health
   // 2-digit hex number (0-255 dec) three times: 56 -> 565656.
   return (brightness * 0x10000) + (brightness * 0x100) + brightness
 }
 
-module.exports = Tile
+module.exports = Animal
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const Phaser = require('./Phaser')
 const Tile = require('./Tile')
+const Animal = require('./Animal')
 
 const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {create, update})
 
 const MAP_SIZE = 50
+const TILE_SIZE = 8
+const ANIMAL_NUMBER = 10
 let tiles = []
+let animals = []
 
 function create () {
   game.scale.scaleMode = Phaser.ScaleManager.RESIZE
@@ -13,17 +17,25 @@ function create () {
   for (let x = 0; x < MAP_SIZE; x++) {
     tiles[x] = []
     for (let y = 0; y < MAP_SIZE; y++) {
-      tiles[x][y] = new Tile(x, y, game)
+      tiles[x][y] = new Tile(x, y, TILE_SIZE, game)
     }
   }
 
-  game.time.events.loop(1000, tick, this)
+  for (let i = 0; i < ANIMAL_NUMBER; i++) {
+    const x = game.math.between(0, MAP_SIZE - 1)
+    const y = game.math.between(0, MAP_SIZE - 1)
+    animals.push(new Animal(x, y, TILE_SIZE, game))
+  }
+
+  game.time.events.loop(1000, tick)
 }
 
 function update () {
   tiles.forEach(row => row.forEach(tile => tile.render()))
+  animals.forEach(a => a.render())
 }
 
 function tick () {
   tiles.forEach(row => row.forEach(tile => tile.tick()))
+  animals.forEach(a => a.tick())
 }
