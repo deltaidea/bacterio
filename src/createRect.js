4eb154fa43a17438977e81c91bfa9a4d30427f0a
Nikita Litvin <n@deltaidea.com>
2016-11-15 11:43:07 +0300

    fix(createRect): add workaround for a memory leak in PIXI

--- src/createRect.js
+++ src/createRect.js
@@ -1,3 +1,13 @@
+// TODO: Memory leak comes from PIXI.CanvasPool.pool (array),
+// which is being filled up by every permutation of bitmaps when they're tinted.
+// Maybe it's cheaper to compute all 256 tints at `create` time and  reuse them.
+// See also: html5gamedevs.com/topic/26018-solved-using-tinted-bitmaptext-causes-memory-leaks-in-canvas
+// Workaround:
+setInterval(() => {
+  global.PIXI.CanvasPool.pool.forEach(e => { delete e.parentNode })
+  global.PIXI.CanvasPool.pool = []
+}, 1000)
+
 module.exports = function createRect (x, y, width, height, color) {
   const bitmap = game.make.bitmapData(width, height)
   bitmap.rect(0, 0, width, height, color)
