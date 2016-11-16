ee2c70e863b4cb35ab3b24f044e942b1855a5d36
Nikita Litvin <n@deltaidea.com>
2016-11-16 14:53:21 +0300

    feat(foodManagement): add more food to punish breeding less

--- src/foodManagement.js
+++ src/foodManagement.js
@@ -6,7 +6,7 @@ module.exports = {
     for (let x = 0; x < game.MAP_SIZE; x++) {
       for (let y = 0; y < game.MAP_SIZE; y++) {
         let tile = game.tiles[x][y]
-        let tileYield = noise.perlin3(x / 10, y / 10, game.yearsRunning / 5) * 0.2
+        let tileYield = noise.perlin3(x / 10, y / 10, game.yearsRunning / 5) * 0.5
         tile.food = game.math.clamp(tile.food + tileYield, 0, 255)
       }
     }
